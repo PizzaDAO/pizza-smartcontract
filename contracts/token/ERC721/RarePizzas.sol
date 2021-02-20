b51a18819fa04cc9087330d4fa5819aaf3a8fe91
antron3000 <fortyonepointsix@gmail.com>
2021-02-20 00:05:45 -0500

    fixed useless constructor

--- contracts/token/ERC721/RarePizzas.sol
+++ contracts/token/ERC721/RarePizzas.sol
@@ -40,9 +40,7 @@ contract RarePizzas is Context, ERC165, IERC721, IERC721Metadata {
     /**
      * @dev Initializes the contract by setting a `name` and a `symbol` to the token collection.
      */
-    constructor (string memory name_, string memory symbol_) {
-        _name = name_;
-        _symbol = symbol_;
+    constructor () {
     }
 
     /**
