c5c1037ce7dedc0ca7ec7aa0d004c63a52b6b735
CryptoSpaces <39212459+CryptoSpaces@users.noreply.github.com>
2021-03-11 18:37:33 +0100

    Update linting config, project config and README (#22)

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,15 +1,17 @@
-import { task } from "hardhat/config";
-import "@nomiclabs/hardhat-waffle";
+import { HardhatUserConfig } from 'hardhat/config'
+import { task } from 'hardhat/config'
+import '@nomiclabs/hardhat-waffle'
+import '@openzeppelin/hardhat-upgrades'
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (args, hre) => {
-  const accounts = await hre.ethers.getSigners();
+task('accounts', 'Prints the list of accounts', async (args, hre) => {
+  const accounts = await hre.ethers.getSigners()
 
   for (const account of accounts) {
-    console.log(account.address);
+    console.log(account.address)
   }
-});
+})
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
@@ -17,24 +19,24 @@ task("accounts", "Prints the list of accounts", async (args, hre) => {
 /**
  * @type import('hardhat/config').HardhatUserConfig
  *  rinkeby: {
-      url:  "http://127.0.0.1:8555", //specify ethereum node endpoint
-      accounts: ["0xc87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3"] //specify privateKey of account
+      url:  'http://127.0.0.1:8555', //specify ethereum node endpoint
+      accounts: ['0xc87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3'] //specify privateKey of account
     }
  */
-module.exports = {
-  defaultNetwork: "hardhat",
+const config: HardhatUserConfig = {
+  defaultNetwork: 'hardhat',
   networks: {
-    hardhat: {
-    },
-
+    hardhat: {},
   },
   solidity: {
-    version: "0.8.0",
+    version: '0.8.0',
     settings: {
       optimizer: {
         enabled: true,
-        runs: 200
-      }
-    }
-  }
-};
+        runs: 200,
+      },
+    },
+  },
+}
+
+export default config
