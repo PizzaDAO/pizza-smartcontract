4255ee6d48d7c9abd7b938e94d363d55868c7fbd
CryptoSpaces <cryptospaces1@gmail.com>
2021-03-11 12:58:11 +0100

    Lint rarepizzabox test

--- scripts/deploy-box.ts
+++ scripts/deploy-box.ts
@@ -1,4 +1,3 @@
-// scripts/create-box.js
 import { ethers, upgrades } from 'hardhat'
 
 async function main() {
--- test/rarepizzasbox.test.ts
+++ test/rarepizzasbox.test.ts
@@ -3,44 +3,50 @@ import { MockProvider } from 'ethereum-waffle'
 import { ethers } from 'hardhat'
 
 describe('test Rare Pizzas Box', function () {
-    let instance: Contract
+  let instance: Contract
 
-    const [wallet] = new MockProvider().getWallets()
+  const [wallet] = new MockProvider().getWallets()
 
-    beforeEach(async () => {
-        const Box = await ethers.getContractFactory('RarePizzasBox')
-        instance = await Box.deploy()
-    })
+  beforeEach(async () => {
+    const Box = await ethers.getContractFactory('RarePizzasBox')
+    instance = await Box.deploy()
+  })
 
-    it('Should return prices for the bonding curve', async () => {
+  it('Should return prices for the bonding curve', async () => {
+    let r = await instance.curve(1)
+    console.log(r.toString() / 10 ** 18)
 
-        let r = await instance.curve(1)
-        console.log(r.toString() / 10 ** 18)
-        r = await instance.curve(10)
-        console.log(r.toString() / 10 ** 18)
-        r = await instance.curve(100)
-        console.log(r.toString() / 10 ** 18)
-        r = await instance.curve(10000)
-        console.log(r.toString() / 10 ** 18)
-        r = await instance.curve(5000)
-        console.log(r.toString() / 10 ** 18)
-        r = await instance.curve(6000)
-        console.log(r.toString() / 10 ** 18)
-        r = await instance.curve(7000)
-        console.log(r.toString() / 10 ** 18)
-        r = await instance.curve(10000)
-        console.log(r.toString() / 10 ** 18)
+    r = await instance.curve(10)
+    console.log(r.toString() / 10 ** 18)
 
-        // TODO: expect
-    })
+    r = await instance.curve(100)
+    console.log(r.toString() / 10 ** 18)
 
-    it('Should allow payments to the payable contract', async () => {
+    r = await instance.curve(10000)
+    console.log(r.toString() / 10 ** 18)
 
-        const price: BigNumber = await instance.getPrice()
-        expect(price.toNumber()).toBe(0)
+    r = await instance.curve(5000)
+    console.log(r.toString() / 10 ** 18)
 
-        await wallet.sendTransaction({ to: instance.address, value: price.toNumber() })
+    r = await instance.curve(6000)
+    console.log(r.toString() / 10 ** 18)
 
-        // TODO: expect
-    })
+    r = await instance.curve(7000)
+    console.log(r.toString() / 10 ** 18)
+
+    r = await instance.curve(10000)
+    console.log(r.toString() / 10 ** 18)
+
+    // TODO: expect
+  })
+
+  it('Should allow payments to the payable contract', async () => {
+    const price: BigNumber = await instance.getPrice()
+
+    expect(price.toNumber()).toBe(0)
+
+    await wallet.sendTransaction({ to: instance.address, value: price.toNumber() })
+
+    // TODO: expect
+  })
 })
