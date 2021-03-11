7350c5d84ae7d8e070641edc2cb17b6a2ab02aa1
CryptoSpaces <cryptospaces1@gmail.com>
2021-03-11 13:44:51 +0100

    Refactor unit tests

--- test/rarepizzasbox.test.ts
+++ test/rarepizzasbox.test.ts
@@ -1,51 +1,39 @@
-import { BigNumber, Contract } from 'ethers'
+import { utils, BigNumber, Contract } from 'ethers'
 import { MockProvider } from 'ethereum-waffle'
 import { ethers } from 'hardhat'
 
-describe('test Rare Pizzas Box', function () {
-  let instance: Contract
+type TestContext = {
+  box: Contract
+}
 
-  const [wallet] = new MockProvider().getWallets()
+const [wallet] = new MockProvider().getWallets()
 
+let testContext: TestContext
+
+describe('Rare Pizzas Box', function () {
   beforeEach(async () => {
     const Box = await ethers.getContractFactory('RarePizzasBox')
-    instance = await Box.deploy()
+    const box = await Box.deploy()
+
+    testContext = {
+      box,
+    }
   })
 
-  it('Should return prices for the bonding curve', async () => {
-    let r = await instance.curve(1)
-    console.log(r.toString() / 10 ** 18)
+  it.each([1, 10, 100, 1000, 5000, 6000, 7000, 10000])('Should return prices for the bonding curve', async tokenId => {
+    const { box } = testContext
+    const r: BigNumber = await box.curve(tokenId)
 
-    r = await instance.curve(10)
-    console.log(r.toString() / 10 ** 18)
-
-    r = await instance.curve(100)
-    console.log(r.toString() / 10 ** 18)
-
-    r = await instance.curve(10000)
-    console.log(r.toString() / 10 ** 18)
-
-    r = await instance.curve(5000)
-    console.log(r.toString() / 10 ** 18)
-
-    r = await instance.curve(6000)
-    console.log(r.toString() / 10 ** 18)
-
-    r = await instance.curve(7000)
-    console.log(r.toString() / 10 ** 18)
-
-    r = await instance.curve(10000)
-    console.log(r.toString() / 10 ** 18)
-
-    // TODO: expect
+    console.log(utils.formatEther(r))
   })
 
   it('Should allow payments to the payable contract', async () => {
-    const price: BigNumber = await instance.getPrice()
+    const { box } = testContext
+    const price: BigNumber = await box.getPrice()
 
     expect(price.toNumber()).toBe(0)
 
-    await wallet.sendTransaction({ to: instance.address, value: price.toNumber() })
+    await wallet.sendTransaction({ to: box.address, value: price.toNumber() })
 
     // TODO: expect
   })
