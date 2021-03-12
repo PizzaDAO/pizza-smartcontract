3da28dd796cf6c06256a0ac17a0022dbf24a4fea
CryptoSpaces <39212459+CryptoSpaces@users.noreply.github.com>
2021-03-12 01:49:47 +0100

    Add additional tests for contracts (#24)

--- scripts/deploy-box.ts
+++ scripts/deploy-box.ts
@@ -1,17 +1,18 @@
-// scripts/create-box.js
 import { ethers, upgrades } from 'hardhat'
 
 async function main() {
   // We get the contract to deploy
   const Box = await ethers.getContractFactory('RarePizzasBox')
   const box = await upgrades.deployProxy(Box)
+
   await box.deployed()
+
   console.log('RarePizzasBox deployed to:', box.address)
 }
 
 main()
   .then(() => process.exit(0))
-  .catch(error => {
+  .catch((error) => {
     console.error(error)
     process.exit(1)
   })
