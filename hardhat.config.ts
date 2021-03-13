222ce313964788324c592aa2b36838ee94ec2931
CryptoSpaces <cryptospaces1@gmail.com>
2021-03-13 16:47:09 +0100

    Add dotenv

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,10 @@
-import { HardhatUserConfig } from 'hardhat/config'
-import { task } from 'hardhat/config'
-import "@nomiclabs/hardhat-etherscan"
+import { task, HardhatUserConfig } from 'hardhat/config'
+import { NetworksUserConfig } from 'hardhat/types'
+import '@nomiclabs/hardhat-etherscan'
 import '@nomiclabs/hardhat-waffle'
 import '@openzeppelin/hardhat-upgrades'
+
+import config, { NetworkConfig } from './config'
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -14,58 +16,48 @@ task('accounts', 'Prints the list of accounts', async (args, hre) => {
   }
 })
 
-// Go to https://www.alchemyapi.io, sign up, create
-// a new App in its dashboard, and replace "KEY" with its key
-const ALCHEMY_API_KEY = "YOUR_API_KEY";
-const ALCHEMY_GOERLI_KEY = "YOUR_API_KEY";
+const networks: NetworksUserConfig = {
+  mainnet: {
+    url: `https://eth-mainnet.alchemyapi.io/v2/${config.ALCHEMY_API_KEY}`,
+    accounts: [`0xNOPE!`],
+  },
+  goerli: {
+    url: `https://eth-goerli.alchemyapi.io/v2/${config.ALCHEMY_GOERLI_KEY}`,
+    accounts: [`0x${config.GOERLI_PRIVATE_KEY}`],
+  },
+  rinkeby: {
+    url: `https://eth-rinkeby.alchemyapi.io/v2/${config.ALCHEMY_API_KEY}`,
+    accounts: [`0x${config.RINKEBY_PRIVATE_KEY}`],
+  },
+  ropsten: {
+    url: `https://eth-ropsten.alchemyapi.io/v2/${config.ALCHEMY_API_KEY}`,
+    accounts: [`0x${config.ROPSTEN_PRIVATE_KEY}`],
+  },
+}
 
-// Your API key for Etherscan
-// Obtain one at https://etherscan.io/
-const ETHERSCAN_API_KEY = "YOUR_API_KEY"
+const getNetworks = (config: NetworkConfig) => {
+  const networkName = config.NETWORK.toLowerCase()
+  const network = networks[networkName]
 
-// Replace these private keys with your testnet account private keys
-// To export your private key from Metamask, open Metamask and
-// go to Account Details > Export Private Key
-// Be aware of NEVER putting real Ether into testing accounts
-const GOERLI_PRIVATE_KEY = "YOUR GOERLI PRIVATE KEY";
-const RINKEBY_PRIVATE_KEY = "YOUR RINKEBY PRIVATE KEY";
-const ROPSTEN_PRIVATE_KEY = "YOUR ROPSTEN PRIVATE KEY";
-
+  return {
+    hardhat: {},
+    ...(network && { [networkName]: network }),
+  }
+}
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
-
 /**
- * @type import('hardhat/config').HardhatUserConfig
  *  rinkeby: {
       url:  'http://127.0.0.1:8555', //specify ethereum node endpoint
       accounts: ['0xc87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3'] //specify privateKey of account
     }
  */
-const config: HardhatUserConfig = {
+const hardhatConfig: HardhatUserConfig = {
   defaultNetwork: 'hardhat',
-  networks: {
-    // goerli: {
-    //   url: `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_GOERLI_KEY}`,
-    //   accounts: [`0x${GOERLI_PRIVATE_KEY}`]
-    // },
-    hardhat: {},
-    // mainnet: {
-    //   url: `https://eth-mainnet.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
-    //   accounts: [`0xNOPE!`]
-    // },
-    // rinkeby: {
-    //   url: `https://eth-rinkeby.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
-    //   accounts: [`0x${RINKEBY_PRIVATE_KEY}`]
-    // },
-    // ropsten: {
-    //   url: `https://eth-ropsten.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
-    //   accounts: [`0x${ROPSTEN_PRIVATE_KEY}`]
-    // }
-  },
+  networks: getNetworks(config),
   etherscan: {
-
-    apiKey: "YOUR_API_KEY"
+    apiKey: config.ETHERSCAN_API_KEY,
   },
   solidity: {
     version: '0.8.0',
@@ -78,4 +70,4 @@ const config: HardhatUserConfig = {
   },
 }
 
-export default config
+export default hardhatConfig
