7987aca1bcefaedb576a02c06f12b67e6dce1b0b
CryptoSpaces <cryptospaces1@gmail.com>
2021-03-13 16:46:52 +0100

    Lint deploy-box.proxy script

--- scripts/deploy-box.proxy.ts
+++ scripts/deploy-box.proxy.ts
@@ -1,14 +1,10 @@
 import { ethers, upgrades } from 'hardhat'
 
 async function main() {
-  const [deployer] = await ethers.getSigners();
+  const [deployer] = await ethers.getSigners()
 
-  console.log(
-    "Deploying contracts with the account:",
-    deployer.address
-  );
-
-  console.log("Account balance:", (await deployer.getBalance()).toString());
+  console.log('Deploying contracts with the account:', deployer.address)
+  console.log('Account balance:', (await deployer.getBalance()).toString())
 
   // We get the contract to deploy
   const Box = await ethers.getContractFactory('RarePizzasBox')
@@ -16,6 +12,7 @@ async function main() {
   // calls RarePizzasBox.initialize()
   // gnosis safe: 0xBA5E28a2D1C8cF67Ac9E0dfc850DC8b7b21A4DE2
   const box = await upgrades.deployProxy(Box)
+
   await box.deployed()
 
   console.log('RarePizzasBox deployed to:', box.address)
