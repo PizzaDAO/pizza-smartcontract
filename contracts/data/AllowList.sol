9b85b41358dcab201e970549e3dd2891f1b12e7f
antron3000 <>
2021-03-14 17:53:47 -0400

    optimize allowList

--- contracts/data/AllowList.sol
+++ contracts/data/AllowList.sol
@@ -5,13 +5,16 @@ pragma solidity ^0.8.0;
 /**
  * @dev a FakeRarePizzasBox is a wrapper exposing modifying contract variables for testing
  */
- contract AllowList {
+contract AllowList {
 
-     mapping(address=>uint) public allowed;
+    address allower;
 
-     function allow(address[] memory allowedAddresses) public {
-         for(uint i = 0;i<allowedAddresses.length;i++){
-             allowed[allowedAddresses[i]] = 10;
-         }
-     }
- }
+    mapping(address=>uint) public allowed;
+
+    function allow(address[] memory allowedAddresses) public {
+        require(msg.sender==allower);
+        for(uint i = 0;i<allowedAddresses.length;i++){
+            allowed[allowedAddresses[i]] = 10;
+        }
+    }
+}
