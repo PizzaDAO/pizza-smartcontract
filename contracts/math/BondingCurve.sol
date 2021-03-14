fbabea9e1a146583ac8f5b549e40b1993083784c
Matt Wilhelm <github@addressxception.com>
2021-03-14 16:50:14 -0400

    add latest bonding curve

--- contracts/math/BondingCurve.sol
+++ contracts/math/BondingCurve.sol
@@ -6,30 +6,33 @@ contract BondingCurve {
     uint256 constant oneEth = 10**18;
     uint256 constant oneGwei = 10**4;
     uint256 constant MAX_CURVE = 8750;
-    uint256 constant TIER0 = (oneEth / oneGwei);
-    uint256 constant TIER1 = ((126 * oneEth) / oneGwei);
-    uint256 constant TIER2 = ((376 * oneEth) / oneGwei);
-    uint256 constant TIER3 = ((1626 * oneEth) / oneGwei);
-    uint256 constant TIER4 = ((2126 * oneEth) / oneGwei);
+    uint256 constant TIER1 = ((501 * oneEth) / 10**4);
+    uint256 constant TIER2 = TIER1 + ((250 * oneEth) / 10**3);
+    uint256 constant TIER3 = TIER2 + ((250 * oneEth) / 10**3);
+    uint256 constant TIER4 = TIER3 + ((250 * oneEth) / 10**3);
+    uint256 constant TIER5 = TIER4 + ((500 * oneEth) / 10**3);
 
     function curve(uint256 n) public pure returns (uint256 price) {
         require(n > 0, 'BondingCurve: starting position cannot be zero');
         require(n <= MAX_CURVE, 'BondingCurve: cannot go past MAX_CURVE value');
-        uint256 nInEth = (n * oneEth);
+        // x+.0001
         if (n <= 2500) {
-            return TIER0 + (nInEth / (2 * 10**5));
+            return ((2 * n * oneEth) / (10**5)) + oneEth / 10**4;
         }
         if (n > 2500 && n <= 5000) {
-            return TIER1 + (nInEth / (10**5));
+            return TIER1 + ((4 * n * oneEth) / (10**5));
         }
         if (n > 5000 && n <= 7500) {
-            return TIER2 + ((5 * nInEth) / (10**5));
+            return TIER2 + ((n * oneEth) / (10**4));
         }
         if (n > 7500 && n <= 8000) {
-            return TIER3 + (nInEth / (10**4));
+            return TIER3 + ((5 * n * oneEth) / (10**4));
         }
-        if (n > 8000) {
-            return TIER4 + ((2 * nInEth) / (10**4));
+        if (n > 8000 && n <= 8500) {
+            return TIER4 + ((n * oneEth) / (10**3));
+        }
+        if (n > 8500 && n <= 8750) {
+            return TIER5 + ((3 * n * oneEth) / (10**3));
         }
     }
 }
