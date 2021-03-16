e99f2d71d0d986a9e5c4314a7d316eeeeb5468f1
Matt Wilhelm <github@addressxception.com>
2021-03-15 22:41:10 -0400

    Add production deployment configutration

---
+++ scripts/box.update.allowed.devs.ts
@@ -0,0 +1,37 @@
+import { ethers, upgrades } from 'hardhat'
+import utils from './utils'
+import config from '../config'
+
+import boxContract from '../artifacts/contracts/token/RarePizzasBox.sol/RarePizzasBox.json';
+
+import presale_devs from './reservations.mainnet.devs.json';
+
+// seed the contract with addresses
+async function main() {
+    const [deployer] = await ethers.getSigners()
+    const provider = new ethers.providers.AlchemyProvider(config.NETWORK, utils.getAlchemyAPIKey(config));
+    const wallet = new ethers.Wallet(utils.getDeploymentKey(config), provider)
+
+    const instanceAddress = utils.getProxyAddress(config)
+
+    console.log('Connecting to instance')
+
+    const contract = new ethers.Contract(instanceAddress, boxContract.abi, wallet);
+
+    const current = await contract.getBitcoinPriceInWei()
+    //verify we can query something
+    console.log(`current BTC-ETH Price: ${current.toString() / 10 ** 18}`)
+
+    console.log('seeding addresses')
+
+    await contract.setPresaleAllowed(3, [...presale_devs])
+
+    console.log('addresses seeded')
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
