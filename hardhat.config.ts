9173c40bb81fc198ae44b394d149ab87b67197c1
John <jjj17@protonmail.com>
2021-03-19 19:32:16 -0400

    VRF integration merge

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -61,13 +61,18 @@ const hardhatConfig: HardhatUserConfig = {
     apiKey: config.ETHERSCAN_API_KEY,
   },
   solidity: {
-    version: '0.8.0',
-    settings: {
+    compilers: [{ version: "0.8.0", settings: {
       optimizer: {
         enabled: true,
         runs: 200,
       },
-    },
+    } }, { version: "0.6.6", settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    }}],
+    
   },
   contractSizer: {
     alphaSort: true,
