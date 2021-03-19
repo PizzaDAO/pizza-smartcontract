150c4f67dc16e68804a74fca2ee00a030d27b944
antron3000 <>
2021-03-19 14:04:46 -0400

    function refactoring

--- contracts/token/RandomConsumer.sol
+++ contracts/token/RandomConsumer.sol
@@ -8,21 +8,21 @@ interface IPizza{
     function MintWithArtwork(bytes32 id,uint random) external;
 }
 contract RandomConsumer is VRFConsumerBase {
-    
+
     bytes32 internal keyHash;
     uint256 internal fee;
     address pizzas;
     uint256 public randomResult;
-    
+
     /**
      * Constructor inherits VRFConsumerBase
-     * 
+     *
      * Network: Kovan
      * Chainlink VRF Coordinator address: 0xdD3782915140c8f3b190B5D67eAc6dc5760C46E9
      * LINK token address:                0xa36085F69e2889c224210F603D836748e7dC0088
      * Key Hash: 0x6c3699283bda56ad74f6b855546325b68d482e983852a7a82979cc4807b641f4
      */
-    constructor(address p) 
+    constructor(address p)
         VRFConsumerBase(
             0xdD3782915140c8f3b190B5D67eAc6dc5760C46E9, // VRF Coordinator
             0xa36085F69e2889c224210F603D836748e7dC0088  // LINK Token
@@ -32,8 +32,8 @@ contract RandomConsumer is VRFConsumerBase {
         fee = 0.1 * 10 ** 18; // 0.1 LINK (varies by network)
         pizzas=p;
     }
-    
-    /** 
+
+    /**
      * Requests randomness from a user-provided seed
      */
     function getRandomNumber() public returns (bytes32 requestId) {
@@ -46,7 +46,7 @@ contract RandomConsumer is VRFConsumerBase {
      * Callback function used by VRF Coordinator
      */
     function fulfillRandomness(bytes32 requestId, uint256 randomness) internal override {
-        
-        IPizza(pizzas).MintWithArtwork(requestId,randomness);
+
+        IPizza(pizzas).mintWithArtwork(requestId,randomness);
     }
 }
