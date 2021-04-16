9ce2b63b804af8e46954aa5e7bf47f96568d2b3f
John <jjj17@protonmail.com>
2021-04-15 20:50:10 -0400

    started purchase flow

--- contracts/slices/sliceERC1155.sol
+++ contracts/slices/sliceERC1155.sol
@@ -4,16 +4,25 @@ pragma solidity ^0.6.0;
 import "@openzeppelin/contracts-upgradeable/token/ERC1155/ERC1155Upgradeable.sol";
 import "@openzeppelin/contracts/token/IERC721.sol";
 
+
+interface RarePizzas is IERC721{
+    function purchase() external;
+    function getPrice() external view returns(uint);
+}
+
 contract slicer1155 is ERC1155Upgradeable {
 
-    IERC721 public rarePizzas;
-    
-    mapping(uint=>uint) public totalSlices;
-    uint public maxSlices=8;
+    RarePizzas public rarePizzas;
 
+    mapping(uint=>uint) public totalSlices;
+    mapping(uint=>uint) public purchasePrice;
+    uint public maxSlices=8;
+    address public lastPurchaser;
+    uint public availablePizzas;
+    uint public currentPizza;
     constructor(address pizzas) public  {
-         __ERC1155_init("pizza slices");
-        rarePizzas(pizzas);
+        __ERC1155_init("pizza slices");
+        rarePizzas=RarePizzas(pizzas);
     }
     function _generateSlice(uint pizza,address receiver) internal {
         require(rarePizzas.owneOf(pizza)==address(this),"pizza must be deposited");
@@ -48,7 +57,26 @@ contract slicer1155 is ERC1155Upgradeable {
         _generateSlices(pizza,msg.sender);
 
     }
-
+    function purchaseSlice() public payable{
+        // require(rarePizzas.owneOf(pizza)==address(this),"pizza must be deposited");
+         bool purchased=availablePizzas>0;
+         if(purchased){
+             uint price=purchasePrice[pizza];
+             require(msg.value>(purchasePrice/8),"");
+             _generateSlice(currentPizza,msg.sender);
+         }else{
+             uint p=rarePizzas.getPrice();
+             require(msg.value>=p/8,"");
+             uint256 startGas = gasleft();
+             uint purchasePrice=purchaseFromCurve();
+             lastPurchaser=msg.sender;
+         }
+        
+    }
+    function purchaseFromCurve() public returns(uint){
+        uint p= rarePizzas.getPrice();
+        rarePizzas.purchase{value:p}();
+    } 
     function burnFromDeposit(uint pizza) public{
          safeTransferFrom(
         msg.sender,
@@ -67,5 +95,13 @@ contract slicer1155 is ERC1155Upgradeable {
         totalSlices[pizza]=0;
     }
     
-    
+     function onERC721Received(address operator, address from, uint256 tokenId, bytes calldata data) external returns (bytes4){
+         if( from==address(0)&& lastPurchaser!=address(0) ){
+              _generateSlice(tokenId,lastPurchaser);
+              lastPurchaser=address(0);
+              currentPizza=tokenId;
+              availablePizzas=7;
+         }
+        return RarePizzas.onERC721Received.selector;
+     }
 }
