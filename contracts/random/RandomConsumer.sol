9970aec9f5f8c4c6b0f519d376afb897bba08c56
Matt Wilhelm <github@addressxception.com>
2021-04-24 20:34:36 -0400

    factor out VRF interfaces

--- contracts/random/RandomConsumer.sol
+++ contracts/random/RandomConsumer.sol
@@ -1,20 +1,13 @@
+// SPDX-License-Identifier: MIT
+
 pragma solidity ^0.6.6;
 
 import '@chainlink/contracts/src/v0.6/VRFConsumerBase.sol';
 import '@openzeppelin/contracts/access/Ownable.sol';
 import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 
+import '../interfaces/IChainlinkVRFCallback.0.6.6.sol';
 import '../interfaces/IChainlinkVRFAdmin.sol';
-
-/**
- * Public interface for interacting with rare pizzas box V2
- */
-interface IChainlinkVRFCallback {
-    /**
-     * Callback function called by the VRF consumer with random response
-     */
-    function fulfillRandomness(bytes32 request, uint256 random) external;
-}
 
 /**
  * Random Consumer contract interacts with chainlink VRF.
