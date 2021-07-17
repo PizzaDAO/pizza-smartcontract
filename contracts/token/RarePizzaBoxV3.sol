a0a8fb242a1ccfb1f33e2bd80da06be716375906
John <jjj17@protonmail.com>
2021-07-17 13:33:38 -0400

    new branch for slices with lotter distribution

--- contracts/token/RarePizzaBoxV3.sol
+++ contracts/token/RarePizzaBoxV3.sol
@@ -30,7 +30,8 @@ contract RarePizzasBoxV3 is RarePizzasBoxV2 {
     address public sliceAddress;
     bool public sliceQueried;
     // END V2 Variables
-
+    mapping(uint=>address[]) public sliceHolders;
+    uint public slicePrice;
     // IChainlinkVRFCallback
 
     function fulfillRandomness(bytes32 request, uint256 random) external override {
@@ -41,15 +42,19 @@ contract RarePizzasBoxV3 is RarePizzasBoxV2 {
        
         uint256 id = _getNextPizzaTokenId();
         if (sliceQuery[request]) {
-            console.log(id,"MINTING NEW SLICE");
-            sliceQueried=false;
-            _mintNewSlice(to, id, random);
+            //uint winner=random % 8;
+            address to =sliceHolders[currentSliceID][random % 8];
+            _safeMint(to, id);
+            _assignBoxArtwork(id, random);
         } else {
             _safeMint(to, id);
             _assignBoxArtwork(id, random);
         }
     }
-    function setSliceAddress(address a) public {
+    function overrideSlice() public onlyOwner(){
+        sliceQueried=false;
+    }
+    function setSliceAddress(address a) public onlyOwner() {
         sliceAddress=a;
     }
     function _mintNewSlice(
@@ -61,9 +66,9 @@ contract RarePizzasBoxV3 is RarePizzasBoxV2 {
         
         //sliceType[id] = pseudoRandom;
         
-        currentSliceID=((slicedBoxes.length+1)*1000)+pseudoRandom;
-        slicedBoxes.push(currentSliceID);
-        availableSlices=7;
+        //currentSliceID=((slicedBoxes.length+1)*1000)+pseudoRandom;
+        //slicedBoxes.push(currentSliceID);
+        //availableSlices=7;
         bool result = ISlice(sliceAddress).externalSliceMint(to, currentSliceID);
         require(result, 'slice minting must work');
     }
@@ -81,31 +86,42 @@ contract RarePizzasBoxV3 is RarePizzasBoxV2 {
                 (_presalePurchaseCount[msg.sender] < _presaleAllowed[msg.sender]),
             "sale hasn't started"
         );
-        require(sliceQueried==false,"a slice is currently queried");
+       // require(sliceQueried==false,"a slice is currently queried");
         require(totalSupply().add(1) <= MAX_TOKEN_SUPPLY - 1, 'exceeds supply.');
-
-        uint256 price = getPrice() / 8;
-        require(msg.value >= price, 'price too low');
-        payable(msg.sender).transfer(msg.value - price);
+        if (availableSlices == 0) {
+            uint256 price = slicePrice / 8;
+          
+            _purchased_pizza_count.increment();
+            currentSliceID= _purchased_pizza_count;
+            sliceHolders[currentSliceID].push(msg.sender);
+             _mintSlice(msg.sender, currentSliceID);
+            
+        }else{
+            uint256 price = slicePrice / 8;
+            require(msg.value >= price, 'price too low');
+            payable(msg.sender).transfer(msg.value - price);
+            sliceHolders[currentSliceID].push(msg.sender);
+            if(availableSlices == 1){
+                _externalSliceQuery();
+            }
+             _mintSlice(msg.sender, currentSliceID);
+        }
+            
+        
+        
 
         // Presale addresses can purchase up to X total
-        if (availableSlices == 0) {
-            _externalSliceQuery(msg.sender);
-            sliceQueried=true;
-            _purchased_pizza_count.increment();
-        } else {
-            _mintSlice(msg.sender, currentSliceID);
-        }
+      
     }
 
     /**
      * override replaces the previous block hash with the difficulty
      */
 
-    function _externalSliceQuery(address to) internal virtual {
+    function _externalSliceQuery() internal virtual {
         if (_chainlinkVRFConsumer != address(0)) {
             try IChainlinkVRFRandomConsumer(_chainlinkVRFConsumer).getRandomNumber() returns (bytes32 requestId) {
-                _purchaseID[requestId] = to;
+                //_purchaseID[requestId] = to;
                 sliceQuery[requestId] = true;
                 return;
             } catch (bytes memory reason) {
