a8aae2b30498ea7a82c056524441d55783a711ae
John <jjj17@protonmail.com>
2021-07-17 13:33:38 -0400

    new branch for slices with lotter distribution

--- test/box.v3.purchase.test.ts
+++ test/box.v3.purchase.test.ts
@@ -94,6 +94,7 @@ describe('Box V3 Purchase Tests', function () {
                 await random.fulfillRandomnessWrapper(testHash, randomNumber('31' + 2, 256, 512))
 
                 price = await boxV3.getPrice()
+                
                 await boxV3.purchaseSlice({ value: price.div(8) })
                 let receipt=await random.fulfillRandomnessWrapper(testHash, randomNumber('31' + 3, 256, 512))
                 console.log(await receipt.wait())
@@ -106,136 +107,16 @@ describe('Box V3 Purchase Tests', function () {
                    
                     console.log(await boxV3.availableSlices())
                     console.log(await boxV3.currentSliceID())
-                    //expect(await box.totalSupply()).to.equal(i + 1)
+                    
                 }
                 await boxV3.purchaseSlice({ value: price.div(8) })
                 await expect(boxV3.purchaseSlice({ value: price.div(8) })).to.be.revertedWith('a slice is currently queried');
+
                 await random.fulfillRandomnessWrapper(testHash, randomNumber('31' + 4, 256, 512))
                 console.log(await boxV3.availableSlices())
                 console.log((await boxV3.currentSliceID()).toString())
             })
         })
     })
-    describe('Purchase a box', () => {
-        describe('Happy flow', () => {
-            it('Should allow purchase of box', async () => {
-                const { box, random, testHash } = testContext
-                const boxBuyers = 10
-
-                for (let i = 0; i < boxBuyers; i++) {
-                    const price: BigNumber = await box.getPrice()
-                    await box.purchase({ value: price.div(8) })
-                    await random.fulfillRandomnessWrapper(testHash, randomNumber('31' + i, 256, 512))
-
-                    expect(await box.totalSupply()).to.equal(i + 1)
-                }
-            })
-
-            it('Should allow purchase of box when VRF unset', async () => {
-                const { box, random, testHash } = testContext
-                const boxBuyers = 10
-
-                // set up the consumer to the 0 address
-                await box.setVRFConsumer('0x0000000000000000000000000000000000000000')
-
-                for (let i = 0; i < boxBuyers; i++) {
-                    const price: BigNumber = await box.getPrice()
-                    await box.purchase({ value: price })
-
-                    // since VRF is unset, should complete mint in the same tx
-                    // DISABLED: await random.fulfillRandomnessWrapper(testHash, 31)
-
-                    expect(await box.totalSupply()).to.equal(i + 1)
-                }
-            })
-
-            it('Should allow purchase of box when VRF is wrong', async () => {
-                const { box, random, testHash, wallet } = testContext
-                const boxBuyers = 10
-
-                // set up the consumer to an invalid contract address
-                await box.setVRFConsumer(box.address)
-
-                for (let i = 0; i < boxBuyers; i++) {
-                    const price: BigNumber = await box.getPrice()
-                    await box.purchase({ value: price })
-
-                    // fulfilling randomness should fail 
-                    // since the random contract is 
-                    // no longer the VRF consumer address
-                    // note in the actual implementation this doesnt get called
-                    // since the purchase function will have already
-                    // fallen through to the old implementation
-                    await expect(random.fulfillRandomnessWrapper(testHash, 31)).to.be.revertedWith('caller not VRF');
-
-                    expect(await box.totalSupply()).to.equal(i + 1)
-                }
-            })
-
-            it('Should still allow owner mint to address using old method', async () => {
-                const { box, wallet } = testContext
-
-                // Make sure balance is 0 before
-                expect(await box.balanceOf(wallet.address)).to.equal(0)
-
-                await box.mint(wallet.address, 1)
-                
-                expect(await box.totalSupply()).to.equal(1)
-                expect(await box.balanceOf(wallet.address)).to.equal(1)
-            })
-
-            it('Should still allow owner purchase to address using old method', async () => {
-                const { box, wallet } = testContext
-                const price: BigNumber = await box.getPrice()
-
-                // Make sure balance is 0 before
-                expect(await box.balanceOf(wallet.address)).to.equal(0)
-
-                await box.purchaseTo(wallet.address,{ value: price })
-
-                expect(await box.totalSupply()).to.equal(1)
-                expect(await box.balanceOf(wallet.address)).to.equal(1)
-            })
-        })
-
-        describe('Revert', () => {
-            it('Should still reject purchase of box for free', async () => {
-                const { box } = testContext
-
-                await expect(box.purchase({ value: 0 })).to.be.reverted
-            })
-
-            // TODO: Implement this with mock contract functions
-            // it('Should reject purchase of box over MAX_NUMBER_OF_BOXES', async (done) => {})
-        })
-    })
-
-    describe('Withdraw funds', () => {
-        describe('Happy flow', () => {
-            it('Should withdraw from owner', async () => {
-                const { box } = testContext
-
-                await expect(box.withdraw()).to.not.be.reverted
-            })
-        })
-
-        describe('Revert', () => {
-            it('Should still not allow purchase for non-presale address', async () => {
-                const { box, wallet } = testContext
-
-                // Make sure balance is 0 before
-                expect(await box.balanceOf(wallet.address)).to.equal(0)
-
-                // Pick a day in the future
-                await box.setSaleStartTimestamp(Date.now() + 24 * 60 * 60 * 1000)
-
-                const price: BigNumber = await box.getPrice()
-
-                await expect(box.purchase({ value: price })).to.be.reverted
-
-                // Check that balance is still 0
-                expect(await box.balanceOf(wallet.address)).to.equal(0)
-            })
-        })
-    })
+   
 })
