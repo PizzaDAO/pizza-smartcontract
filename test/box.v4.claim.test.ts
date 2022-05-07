b440a0246b6645ed6175efac1b648fefdb1952a5
johhonn <jjj.may377@gmail.com>
2022-05-07 17:37:07 -0400

    add merkle tree things and a test

---
+++ test/box.v4.claim.test.ts
@@ -0,0 +1,104 @@
+import { expect } from 'chai'
+import { BigNumber, Contract, Signer, Wallet } from 'ethers'
+import { ethers, upgrades } from 'hardhat'
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers'
+import { MockProvider } from 'ethereum-waffle'
+import { merkleTree as utils } from './helpers'
+
+import config, { NetworkConfig } from '../config'
+
+type TestContext = {
+    box: Contract
+    boxV4: Contract
+    random: Contract
+    accounts: SignerWithAddress[]
+
+}
+
+let testContext: TestContext
+
+let getRinkebyRandomConsumer = async (box: Contract) => {
+    const RandomConsumer = await ethers.getContractFactory('FakeRandomConsumer')
+    return await RandomConsumer.deploy(
+        config.CHAINLINK_RINKEBY_VRF_COORD,
+        config.CHAINLINK_RINKEBY_TOKEN,
+        '0x6c3699283bda56ad74f6b855546325b68d482e983852a7a82979cc4807b641f4',
+        config.CHAINLINK_RINKEBY_VRF_FEE,
+        box.address,
+    )
+}
+
+describe('Box V3 Real Upgrade Tests', function () {
+    beforeEach(async () => {
+        // Deploy the original contract
+        const accounts = await ethers.getSigners()
+        const Box = await ethers.getContractFactory('RarePizzasBox')
+        const box = await upgrades.deployProxy(Box, ['0x0000000000000000000000000000000000000000'])
+        console.log(upgrades)
+        // pick a date like jan 1, 2021
+        await box.setSaleStartTimestamp(1609459200)
+
+        // Call a function that changes state
+        const price: BigNumber = await box.getPrice()
+        await box.purchase({ value: price })
+
+        expect(await box.totalSupply()).to.equal(1)
+        const random = await getRinkebyRandomConsumer(box)
+
+        // run the upgrade
+        const BoxV2 = await ethers.getContractFactory('RarePizzasBoxV2')
+        const BoxV3 = await ethers.getContractFactory('RarePizzasBoxV3')
+        const BoxV4 = await ethers.getContractFactory('RarePizzasBoxV4')
+        const boxV2 = await upgrades.upgradeProxy(box.address, BoxV2)
+        const boxV3 = await upgrades.upgradeProxy(boxV2.address, BoxV3)
+        const boxV4 = await upgrades.upgradeProxy(boxV3.address, BoxV4)
+
+
+        // set up the random consumer
+        await boxV4.setVRFConsumer(random.address)
+        testContext = {
+            box: box,
+            boxV4: boxV4,
+            random: random,
+            accounts
+        }
+    })
+
+    it('can set merkle roots and make a claim', async () => {
+        const { boxV4, accounts, random } = testContext
+        let Tree = utils.merkleTree
+        await boxV4.setSaleWhitelist(Tree.root)
+        await boxV4.setclaimWhiteList(Tree.root2)
+        let proof = Tree.tree2.getProof(Tree.elements2[1])
+        proof = proof.map((item: any) => '0x' + item.data.toString('hex'))
+        await boxV4.connect(accounts[1]).claim(proof, 2)
+
+        await random.fulfillRandomnessWrapper('0x6c3699283bda56ad74f6b855546325b68d482e983852a7a82979cc4807b641f4', 234324)
+        let r = await boxV4.completeClaim('0x6c3699283bda56ad74f6b855546325b68d482e983852a7a82979cc4807b641f4')
+        console.log(await r.wait())
+
+    })
+
+    /**  it('Should not modify ownable when upgrading', async () => {
+            const { box, wallet, signer } = testContext
+    
+            expect(await box.owner()).to.equal(signer.address);
+    
+            // transfer ownership of the proxy
+            await upgrades.admin.transferProxyAdminOwnership(wallet.address)
+            expect(await box.owner()).to.equal(signer.address); // the signer is still the owner
+    
+            // transfer ownership of the contract logic to another wallet
+            await box.connect(signer).transferOwnership(wallet.address)
+            expect(await box.owner()).to.equal(wallet.address)
+    
+            // run the upgrade (using the new owner)
+            const BoxV2 = await ethers.getContractFactory('RarePizzasBoxV2', wallet)
+            const BoxV2Address = await upgrades.prepareUpgrade(box.address, BoxV2)
+            await upgrades.upgradeProxy(box.address, BoxV2)
+    
+            // validate the owner is not changed
+            expect(await box.owner()).to.equal(wallet.address)
+        })
+        **/
+})
---
+++ test/helpers/merkleTree.ts
@@ -0,0 +1,96 @@
+let { MerkleTree } = require("merkletreejs");
+let keccak256 = require("keccak256");
+let ethers = require("ethers");
+const users = [
+    {
+        address: "0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266",
+    },
+    {
+        address: "0x70997970c51812dc3a010c7d01b50e0d17dc79c8",
+    },
+    {
+        address: "0x3c44cdddb6a900fa2b585dd299e03d12fa4293bc",
+    },
+    {
+        address: "0x90f79bf6eb2c4f870365e785982e1f101e93b906",
+    },
+    {
+        address: "0x15d34aaf54267db7d7c367839aaf71a00a2c6a65",
+    },
+    {
+        address: "0x9965507d1a55bcc2695c58ba16fb37d819b0a4dc",
+    },
+    {
+        address: "0x976ea74026e726554db657fa54763abd0c3a0aa9",
+    },
+    {
+        address: "0x14dc79964da2c08b23698b3d3cc7ca32193d9955"
+    },
+    {
+        address: "0x23618e81e3f5cdf7f54c3d65f7fbc0abf5b21e8f",
+    },
+    {
+        address: "0xa0ee7a142d267c1f36714e4a8f75612f20a79720",
+    },
+    {
+        address: "0xbcd4042de499d14e55001ccbb24a551f3b954096",
+    },
+    {
+        address: "0x71bE63f3384f5fb98995898A86B02Fb2426c5788"
+    },
+    {
+        address: "0xfabb0ac9d68b0b445fb7357272ff202c5651694a",
+    },
+    {
+        address: "0x1cbd3b2770909d4e10f157cabc84c7264073c9ec",
+    },
+    {
+        address: "0xdf3e18d64bc6a983f673ab319ccae4f1a57c7097",
+    },
+    {
+        address: "0xcd3b766ccdd6ae721141f452c550ca635964ce71",
+    },
+    {
+        address: "0x2546bcd3c84621e976d8185a91a922ae77ecec30"
+    },
+    {
+        address: "0xbda5747bfd65f08deb54cb465eb87d40e51b197e"
+    },
+    {
+        address: "0xdd2fd4581271e230360230f9337d5c0430bf44c0"
+    },
+    {
+        address: "0x8626f6940e2eb28930efb4cef49b2d1f2c9c1199"
+    },
+];
+
+const elements = users.map((x) =>
+    ethers.utils.solidityKeccak256(["address"], [x.address])
+);
+const elements2 = users.map((x) =>
+    ethers.utils.solidityKeccak256(['address', 'uint256'], [x.address, 2])
+);
+const tree = new MerkleTree(elements, keccak256, { sort: true });
+const tree2 = new MerkleTree(elements2, keccak256, { sort: true });
+export const merkleTree = { tree: tree, tree2: tree2, root: tree.getHexRoot(), root2: tree2.getHexRoot(), elements: elements, elements2: elements2 }
+
+interface claim {
+    address: string,
+    amount: number
+}
+export const generateUserProof = async (claimList: claim[], wallet: string) => {
+    const indexOfUser = claimList
+        .map((x) => x['address'].toLowerCase())
+        .indexOf(wallet.toLowerCase());
+    const claimListHashes = claimList.map((x) =>
+        ethers.utils.solidityKeccak256(['address', 'uint256'], [x['address'].toLowerCase(), x['amount']])
+    );
+    const claimListMerkleTree = new MerkleTree(claimListHashes, keccak256, { sort: true });
+    let proof = claimListMerkleTree.getProof(claimListHashes[indexOfUser]);
+    proof = proof.map((item: any) => '0x' + item.data.toString('hex'));
+}
+// get the root
+// const root = merkleTree.getHexRoot();
+// const indexOfUser = 0;
+// const leaf = elements[indexOfUser];
+// const proof = merkleTree.getHexProof(leaf);
