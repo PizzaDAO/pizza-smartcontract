f679adf7e12faab64cc8f92e89267f289d5d24d4
Alex Jewell <alex.jewell@polysign.io>
2022-05-17 00:15:26 -0500

    Fix webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -19,8 +19,12 @@ module.exports = {
     resolve: {
         extensions: [ '.ts', '.js' ],
         fallback: {
+            "crypto": require.resolve("crypto-browserify"),
             "stream": require.resolve("stream-browserify"),
-            "buffer": require.resolve("buffer/")
+            "buffer": require.resolve("buffer/"),
+            "http": require.resolve("stream-http"),
+            "https": require.resolve("https-browserify"),
+            "os": require.resolve("os-browserify/browser")
         }
     },
 
