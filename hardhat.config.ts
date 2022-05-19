2ee8d5cb307ddff6b254e74605127c4cdca2ce11
AddressXception <github@addressxception.com>
2022-05-19 15:14:11 -0400

    remove unneeded task

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -7,16 +7,6 @@ import 'hardhat-gas-reporter'
 import 'hardhat-contract-sizer'
 import config, { NetworkConfig } from './config'
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task('accounts', 'Prints the list of accounts', async (args, hre) => {
-  const accounts = await hre.ethers.getSigners()
-
-  for (const account of accounts) {
-    console.log(account.address)
-  }
-})
-
 const networks: NetworksUserConfig = {
   mainnet: {
     url: `https://eth-mainnet.alchemyapi.io/v2/${config.ALCHEMY_MAINNET_KEY}`,
