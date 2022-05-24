ade98de18ac9e12a0934d4819f7198fae89a17f6
johhonn <jjj.may377@gmail.com>
2022-05-23 21:01:58 -0400

    claim update compiles

--- contracts/token/V4/RarePizzasBoxV4-2.sol
+++ contracts/token/V4/RarePizzasBoxV4-2.sol
@@ -6,24 +6,21 @@ import '@openzeppelin/contracts-upgradeable/utils/math/SafeMathUpgradeable.sol';
 import '@openzeppelin/contracts-upgradeable/utils/AddressUpgradeable.sol';
 import '@openzeppelin/contracts-upgradeable/utils/CountersUpgradeable.sol';
 
-import { RarePizzasBoxV3Fix } from './RarePizzasBoxV3.sol';
+import { RarePizzasBoxV4 } from './RarePizzasBoxV4.sol';
 import 'hardhat/console.sol';
 
-contract RarePizzasBoxV42 is RarePizzasBoxV3Fix {
+contract RarePizzasBoxV42 is RarePizzasBoxV4 {
     using AddressUpgradeable for address;
     using CountersUpgradeable for CountersUpgradeable.Counter;
     using SafeMathUpgradeable for uint256;
     //address minter;
-    mapping(address => bool) public isMinter;
+    mapping(uint256 => bool) public claimed;
+    function claim(uint256 id) public virtual {
+        require(ownerOf(id)==msg.sender,'sender must own for a box');
+        require(claimed[id]==false,'id has been claimed');
+        require(id<1560,'invalid box for claim');
+        claimed[id]=true;
+        _queryForClaim(msg.sender, 1);
 
-    function mint(address to, uint256 random) external virtual {
-        require(isMinter[msg.sender] == true, 'only can be called minted by authorized contract');
-        uint256 id = _getNextPizzaTokenId();
-        _safeMint(to, id);
-        _assignBoxArtwork(id, random);
-    }
-
-    function toggleMinter(address a) public onlyOwner {
-        isMinter[a] = !isMinter[a];
     }
 }
