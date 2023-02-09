ea6edc5c495e1a1ba12365260e4e057cc3cdfdf3
Daniel vdg <danielvandeghinste@gmail.com>
2023-02-09 17:10:29 +0100

    trying to get ethers importing to scripts

--- scripts/orderAPIConsumer.deploy.ts
+++ scripts/orderAPIConsumer.deploy.ts
@@ -6,29 +6,30 @@ import config, { NetworkConfig } from '../config'
 
 // deploy the OderAPIConsumer
 async function main() {
-    const [deployer] = await ethers.getSigners()
+  const [deployer] = await ethers.getSigners()
 
-    console.log('Preparing RandomConsumer with the account:', deployer.address)
-    console.log('Account balance:', (await deployer.getBalance()).toString())
+  console.log('Preparing RandomConsumer with the account:', deployer.address)
+  console.log('Account balance:', (await deployer.getBalance()).toString())
 
-    const Contract = await ethers.getContractFactory('OrderAPIConsumer')
-    const contract = await Contract.deploy(
-        utils.getChainlinkToken(config),
-        utils.getOrderAPIOracleContractAddress(config),
-        utils.getOrderApiConsumerAuthorizedRequestorAddress(config),
-        utils.getRarePizzasProxyAddress(config),
-        utils.getOrderAPIJobId(config),
-        utils.getOrderAPIJobFee(config))
+  const Contract = await ethers.getContractFactory('OrderAPIConsumer')
+  const contract = await Contract.deploy(
+    utils.getChainlinkToken(config),
+    utils.getOrderAPIOracleContractAddress(config),
+    utils.getOrderApiConsumerAuthorizedRequestorAddress(config),
+    utils.getRarePizzasProxyAddress(config),
+    utils.getOrderAPIJobId(config),
+    utils.getOrderAPIJobFee(config),
+  )
 
-    // TODO: fund with link?
+  // TODO: fund with link?
 
-    console.log('OderAPIConsumer:', contract)
-    // TODO: distro stuff
+  console.log('OderAPIConsumer:', contract)
+  // TODO: distro stuff
 }
 
 main()
-    .then(() => process.exit(0))
-    .catch((error) => {
-        console.error(error)
-        process.exit(1)
-    })
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
