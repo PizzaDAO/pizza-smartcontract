f36231b316d46d422f671c7553cd2a1c28c364cd
Matt Wilhelm <github@addressxception.com>
2023-03-26 14:33:20 -0400

    handle pizza pickups

--- scripts/pizza.assignArtwork.ts
+++ scripts/pizza.assignArtwork.ts
@@ -1,4 +1,4 @@
-import { ethers, upgrades } from 'hardhat'
+import { ethers } from 'hardhat'
 //import { BigNumber, Contract, Wallet } from 'ethers'
 import utils from './utils'
 import config from '../config'
@@ -6,10 +6,9 @@ import config from '../config'
 import Contract from '../artifacts/contracts/token/RarePizzas.sol/RarePizzas.json';
 import { privateEncrypt } from 'crypto';
 
-// set the pizza sale to active
 async function main() {
-    const tokenId = 571
-    const truncated_hex = "17d5dcaa0433cb2370aabdac2215dab7be00d01d3c937687358352d0b6b9d57b"
+    const tokenId = 704
+    const truncated_hex = "0x6aec5b7ee656b16b16048580f39ae8b9cf68dde65add62de70d5f3cc69dbfe17"
 
     const provider = new ethers.providers.AlchemyProvider(config.NETWORK, utils.getAlchemyAPIKey(config));
     const wallet = new ethers.Wallet(utils.getDeploymentKey(config), provider)
@@ -19,9 +18,9 @@ async function main() {
     console.log('Connecting to instance')
     const contract = new ethers.Contract(instanceAddress, Contract.abi, wallet);
 
-    const hex_string = ethers.utils.formatBytes32String()
     console.log('setting artwork')
-    const tx = await contract.setPizzaArtworkURI({type: 0, gasLimit: 120000})
+    //{type: 0,gasPrice: 105000000000, gasLimit: 50000}
+    const tx = await contract.setPizzaArtworkURI(tokenId, truncated_hex)
     console.log(tx)
 
     console.log('complete')
