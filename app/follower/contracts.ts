eb58bf402407ae2e7d2fb958ecce0ea6665da8df
AddressXception <github@addressxception.com>
2023-03-30 16:56:06 -0400

    remove hardcoded ABIs

--- app/follower/contracts.ts
+++ app/follower/contracts.ts
@@ -1,27 +1,28 @@
-import { abi as consumerAbi } from "../OrderAPIConsumer.json";
-import { abi as oracleAbi } from "../OrderAPIOracle.json";
-import { ethers } from "ethers";
+import { ethers } from 'ethers'
+
+import { abi as oracleAbi } from '../../artifacts/contracts/chainlink/OrderAPIOracle.sol/OrderAPIOracle.json'
+import { abi as consumerAbi } from '../../artifacts/contracts/chainlink/OrderAPIConsumer.sol/OrderAPIConsumer.json'
 
 export const provider = new ethers.providers.AlchemyProvider(
   'mainnet',
   process.env.ALCHEMY_MAINNET_KEY,
-);
+)
 
 export const orderApiConsumer = new ethers.Contract(
   process.env.RAREPIZZAS_ORDER_API_CONSUMER_MAINNET_CONTRACT_ADDRESS!,
   consumerAbi,
-  provider
-);
+  provider,
+)
 
 export const orderApiOracle = new ethers.Contract(
   process.env.RAREPIZZAS_ORDER_API_MAINNET_ORACLE_CONTRACT_ADDRESS!,
   oracleAbi,
-  provider
-);
+  provider,
+)
 
 export const contracts = {
   orderApiConsumer,
   orderApiOracle,
-};
+}
 
-export default contracts;
+export default contracts
