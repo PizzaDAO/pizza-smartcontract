8d22f6cc560673fd18cd1ace9cfc89ebfd922045
AddressXception <github@addressxception.com>
2023-03-30 16:49:06 -0400

    refactor paths and move some interfaces
    add readme and rename the command file to index so it can be invoked directly from the command line

---
+++ app/follower/index.ts
@@ -0,0 +1,73 @@
+#!/usr/bin/env ts-node
+import 'dotenv/config'
+import { Command } from 'commander'
+import {
+  ListenOptions,
+  listenRequests,
+  fetchRequests,
+  FetchOptions,
+  pushRequests,
+  PushOptions,
+} from './commands'
+
+//interface Program extends Command, Options {}
+
+const program = new Command() // as Program;
+
+program
+  .version('0.0.1', '-v --version', 'output the current version')
+  .description(
+    'A blockchain event processor which fetchs events in batches and saves ' +
+      'relevant data to file, post saved data to an API, or run as a service ' +
+      'to process events as they are emitted and post them to an API live.',
+  )
+
+program
+  .command('listen', { isDefault: true })
+  .description(
+    'Listens for events from the blockchain, saves them to file,' + ' and posts them to the API',
+  )
+  .action((options: ListenOptions) => {
+    console.log('Listening for events...')
+    listenRequests(options)
+  })
+
+program
+  .command('push')
+  .description(
+    'Pushes data in the data directory to the API.' + ' Can push all data or a specific file.',
+  )
+  .option('-u, --url [url]', 'URL of the api to post request data to', 'http://localhost:8000')
+  .option(
+    '-a, --apiVersion <apiVersion>',
+    'API version, to use when contructing URI to post request data to',
+    'v1',
+  )
+  .action(async (options: PushOptions) => {
+    console.log('Pushing data to API...')
+    await pushRequests(options)
+  })
+
+program
+  .command('fetch')
+  .description(
+    'Fetches data from the API and saves it to the data directory. ' +
+      'Can fetch from a specified block number or a high-water mark block ' +
+      'specified in the fromBlock.json file.',
+  )
+  .option(
+    '-f, --file [file]',
+    'fromBlock value to be used, supplied by a file for persistance',
+    'fromBlock.json',
+  )
+  .option(
+    '-b, --block [block]',
+    'fromBlock value if latestBlock.json not found, otherwise zero if neither value is provided',
+    '0',
+  )
+  .action(async (options: FetchOptions) => {
+    console.log('Fetching data from blockchain...')
+    await fetchRequests(options)
+  })
+
+program.parse()
---
+++ app/follower/types/IOracleRequestData.ts
@@ -0,0 +1,9 @@
+// The decoded oracle request data structure
+
+export interface IOracleRequestData {
+  address: string
+  requestor: string
+  requestId: string
+  token_id: number
+  recipe_id: number
+}
